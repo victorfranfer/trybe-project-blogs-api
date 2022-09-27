41ffb373cd19f13be51f3f079c5ef150dc014a79
VictorFranciscoFerreira <victorfranciscoferreira@gmail.com>
2022-09-27 18:33:32 -0300

    req2:feito

---
+++ src/models/User.js
@@ -0,0 +1,24 @@
+const UserSchema = (sequelize, DataTypes) => {
+  const UserTable = sequelize.define('User', {
+    id: DataTypes.INTEGER,
+    displayName: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,
+    image: DataTypes.STRING,
+  },
+  {
+    tableName: 'users',
+    underscored: true
+  });
+
+  UserTable.associate = (models) => {
+    UserTable.hasMany(models.BlogPosts, {
+      as: 'blogPosts',
+      foreignKey: 'userId'
+    })
+  }
+
+  return UserTable;
+}
+
+module.exports = UserSchema;
