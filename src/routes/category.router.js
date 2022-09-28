75794ac2e108577df9a6c177590badb8362afb29
VictorFranciscoFerreira <victorfranciscoferreira@gmail.com>
2022-09-27 21:19:16 -0300

    req8:feito

---
+++ src/routes/category.router.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const categoryController = require('../controllers/category.controller');
+const validate = require('../middlewares/validateCategory.js');
+const validateToken = require('../middlewares/validateToken');
+
+const routers = express.Router();
+
+routers.post('/', validateToken, validate, categoryController.createCategory);
+
+module.exports = routers;
---
+++ src/services/category.service.js
@@ -0,0 +1,11 @@
+const { Category } = require('../models');
+
+const createCategory = async (name) => {
+  const result = await Category.create({ name });
+
+  return result;
+};
+
+module.exports = {
+  createCategory,
+};
